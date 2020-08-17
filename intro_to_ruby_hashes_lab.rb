1d2b44017a405b35698a3aa5a80096f05f044103
Sean LaFlam <sean.laflam@gmail.com>
2020-08-17 20:53:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,11 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  {railroad: {}}
+  {railroad: {
+    pieces: 4,
+    rent_in_dollars: {},
+    names: {}
+  }}
 end
 
 def monopoly_with_third_tier
